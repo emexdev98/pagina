3ced09508d90ffada080a75ee5e1d1460574b94c
emexdev98 <135179226+emexdev98@users.noreply.github.com>
2023-06-21 19:24:34 -0300

    Update java.js

--- java.js
+++ java.js
@@ -2,12 +2,28 @@ function quedia(){
     var dia= new Date().getDay();
 
     switch (dia){
-        case (2):
-            document.getElementById("teste").innerHTML="Hoje é Terça-feira."
+        case (0):
+            document.getElementById("teste").innerHTML="Hoje é Domingo."
             break;
-            case (3):
+        case (1):
+             document.getElementById("teste").innerHTML="Hoje é Segunda-feira"
+            break;
+        case (2):
+             document.getElementById("teste").innerHTML="Hoje é Terça-feira"
+            break;
+        case (3):
              document.getElementById("teste").innerHTML="Hoje é Quarta-feira"
             break;
+        case (4):
+             document.getElementById("teste").innerHTML="Hoje é Quinta-feira"
+            break;
+        case (5):
+             document.getElementById("teste").innerHTML="Hoje é Sexta-feira"
+            break;
+        case (6):
+             document.getElementById("teste").innerHTML="Hoje é Sábado"
+            break;
+            
             default:
                 document.getElementById("teste").innerHTML= "Não sei que dia é hoje.."
 
