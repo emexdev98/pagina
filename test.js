b6ca558afeb7a35c88c3284387cf57c5dc9de4bb
emexdev98 <135179226+emexdev98@users.noreply.github.com>
2023-06-22 22:32:27 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-for (let i=2023; i>=1900; i--){
+var ano =new Date().getFullYear();
+for (let i=ano; i>=1900; i--){
     document.getElementById("ano").innerHTML +="<option value '"+i+"'>"+i+"</option>"
 }
